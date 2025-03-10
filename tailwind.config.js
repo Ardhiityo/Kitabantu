5cbd5c497cb3605fba71bde3146c21bb8c690a58
belajar-vue-2 <aryaadi229@gmail.com>
2025-03-10 21:17:47 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        popins: "Poppins, sans-serif",
+      },
+      colors: {
+        primary: "#FE9AA0",
+      },
+    },
+  },
+  plugins: [],
+};
